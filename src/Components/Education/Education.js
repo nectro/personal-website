42919a349b629037b571fc4c7625d63c64c6a607
nectro <ss9261@srmist.edu.in>
2022-01-15 01:00:10 +0530

    add animations to educations section

--- src/Components/Education/Education.js
+++ src/Components/Education/Education.js
@@ -1,35 +1,56 @@
 import React from 'react'
 import styles from './Education.module.css'
 import FancyImg from '../../assets/img4.svg'
+import Fade from 'react-reveal/Fade';
+import Bounce from 'react-reveal/Bounce';
+import LightSpeed from 'react-reveal/LightSpeed';
 
 const Education = ()=>{
     return(
         <div className={styles.majorContainer} id="Ed">
             <div className={styles.info}>
                 <div className={styles.heading}>
-                    <div className={styles.word}><p>EDUCATION</p></div>
-                    <div className={styles.line}></div>
+                    <Fade left duration={1000}>
+                        <div className={styles.word}><p>EDUCATION</p></div>
+                    </Fade>
+                    <Fade delay={500}>
+                        <div className={styles.line}></div>
+                    </Fade>
                 </div>
                 <div className={styles.vecImg} id={styles.forMob}>
                     <img src={FancyImg} />
                 </div>
                 <div className={styles.educationInfo}>
-                    <h2>COLLEGE</h2>
+                    <Bounce left>
+                        <h2>COLLEGE</h2>
+                    </Bounce>
                     <div>
-                        <h3>SRM INSTITUTE OF SCIENCE AND TECHNOLOGY</h3>
-                        <p>(2020-PRESENT)</p>
+                        <Fade bottom>
+                            <h3>SRM INSTITUTE OF SCIENCE AND TECHNOLOGY</h3>
+                        </Fade>
+                        <Fade top>
+                            <p>(2020-PRESENT)</p>
+                        </Fade>
                     </div>
                 </div>
                 <div className={styles.educationInfo}>
-                    <h2>SCHOOL</h2>
+                    <Bounce left>
+                        <h2>SCHOOL</h2>
+                    </Bounce>
                     <div>
-                        <h3>METHODIST SCHOOL</h3>
-                        <p>(2005-2020)</p>
+                        <Fade bottom>
+                            <h3>METHODIST SCHOOL</h3>
+                        </Fade>
+                        <Fade top>
+                            <p>(2005-2020)</p>
+                        </Fade>
                     </div>
                 </div>
             </div>
             <div className={styles.vecImg}  id={styles.forComp}>
-                <img src={FancyImg} />
+                <LightSpeed right>
+                    <img src={FancyImg} />
+                </LightSpeed>
             </div>
         </div>
     )
